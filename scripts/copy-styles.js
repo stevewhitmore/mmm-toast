0224c566ae9256b1ceb48dd26274d6e2ceec264e
stevewhitmore <s25w89@gmail.com>
2022-10-21 07:44:56 -0500

    Rolled some stuff back. I guess I knew what I was doing before..

--- scripts/copy-styles.js
+++ scripts/copy-styles.js
@@ -4,6 +4,10 @@ ncp.limit = 1;
 
 let destinationPath = 'dist/mmm-toast/lib/styles';
 
+if (process.argv[2] === 'demo') {
+  destinationPath = 'src/assets'
+}
+
 ncp('projects/mmm-toast/src/lib/styles', destinationPath, (err) => {
   if (err) return console.log(err);
   console.log(`******************* styles successfully copied to ${destinationPath} folder *******************`);
