a9112510656e8a03c91ed78948b84340087d3a78
stevewhitmore <s25w89@gmail.com>
2022-10-26 22:05:23 -0500

    Fixed dependency issue and broke out html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,7 @@ import { MmmToastService } from 'projects/mmm-toast/src/public-api'
 
 @Component({
   selector: 'app-root',
-  template: `
-    <section>
-
-    <header>
-      <div>
-        <h1>mmm-toast Demo</h1>
-        <p>Easy toast notification for Angular</p>
-      </div>
-    </header>
-
-    <app-home></app-home>
-
-    <footer>
-      Released under the <a href="https://gitlab.com/stevewhitmore/mmm-toast/blob/master/LICENSE">MIT</a> license. <a
-        href="https://gitlab.com/stevewhitmore/mmm-toast">View source</a>.
-    </footer>
-
-    <mmm-toast></mmm-toast>
-
-    </section>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
